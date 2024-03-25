08104b4b9383026a872fa40cef0e506e7f5e8f0b
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-03-25 15:51:57 +0000

    Fist commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,31 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
 
-	fetch('https://playground.4geeks.com/apis/fake/todos/user/frankielee2272', {
-      method: "PUT",
-      body: JSON.stringify(todos),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // Will be true if the response is successful
-        console.log(resp.status); // The status code=200 or code=400 etc.
-        console.log(resp.text()); // Will try to return the exact result as a string
-        return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
-    })
-    .then(data => {
-        // Here is where your code should start after the fetch finishes
-        console.log(data); // This will print on the console the exact object received from the server
-    })
-    .catch(error => {
-        // Error handling
-        console.log(error);
-    });
+	useEffect(()=>{
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/frankielee2272')
+		
+		.then(resp =>  resp.json() // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+		)
+	
+	
+		.then(data => {
+			// Here is where your code should start after the fetch finishes
+			console.log(data); // This will print on the console the exact object received from the server
+setTodos(data)
+		})
+		.catch(error => {
+			// Error handling
+			console.log(error);
+		});
+	},[])
+
+	function AddToDo(NewToDo){
+		let NewToDoList=[...todos, {label: NewToDo,done:false}]
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/frankielee2272', {
+			method: "PUT",
+			body: JSON.stringify(NewToDoList),
+			headers: {
+			  "Content-Type": "application/json"
+			}
+		  })
+		  .then(resp =>  resp.json() // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+		  )
+	  
+	  
+		  .then(data => {
+			  // Here is where your code should start after the fetch finishes
+			  console.log("Added todo",data); // This will print on the console the exact object received from the server
+  setTodos(NewToDoList)
+  setInputValue("")
+		  })
+		  .catch(error => {
+			  // Error handling
+			  console.log("Error adding todo",error);
+		  });
+	}
+
+	function DeleteToDo(Index){
+		let NewToDoList=todos.filter((todo,currentIndex)=>Index!==currentIndex)
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/frankielee2272', {
+			method: "PUT",
+			body: JSON.stringify(NewToDoList),
+			headers: {
+			  "Content-Type": "application/json"
+			}
+		  })
+		  .then(resp =>  resp.json() // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+		  )
+		  .then(data => {
+			  // Here is where your code should start after the fetch finishes
+			  console.log("Delete todo",data); // This will print on the console the exact object received from the server
+              setTodos(NewToDoList)
+		  })
+		  .catch(error => {
+			  // Error handling
+			  console.log("Error Deleting todo",error);
+		  });
+	}
+
 	return (
 		<div className="container">
 			<h1>
@@ -41,7 +88,7 @@ const Home = () => {
 						value={inputValue}
 						onKeyDown={(e) => {
 							if (e.key === "Enter") {
-								setTodos(todos.concat({task: inputValue, date: new Date()}));
+								AddToDo(inputValue)
 								setInputValue(""); // Clear the input value
 							}
 						}}
@@ -49,9 +96,10 @@ const Home = () => {
 					/>
 				</li>
 				{todos.map((t, index) => (
-					<li style={{ display: "flex", justifyContent: "space-between" }}>
-						{t.task} - {t.date.toLocaleString()}
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16" onClick={() => setTodos(todos.filter((_, currentIndex) => index !== currentIndex))}>
+					<li key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+						{t.label} 
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16" 
+						onClick={() =>DeleteToDo(index)}>
 							<path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
 						</svg>
 					</li>
